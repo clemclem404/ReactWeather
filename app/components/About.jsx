5c56fcaa153ec423e45427ec6dbee8c5b6aa843b
paulinerousseau <pauline_rousseau@hotmail.co.uk>
2016-10-18 12:03:32 -0400

    sytle about page and link to resources

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,5 +1,6 @@
 var React = require ('react');
 
+
 //var About = React.createClass({
 //render: function() {
 //return (
@@ -11,8 +12,16 @@ var React = require ('react');
 var About = (props) => {
 return (
 <div>
-<h3>About</h3>
-<p>Welcome to the About Page !</p>
+<h1 className="text-center">About</h1>
+<p>Here are few links you could be interested in :</p>
+<ul>
+  <li>
+    <a href="https://facebook.github.io/react">React</a> - This was the JavaScript framework used.
+  </li>
+  <li>
+    <a href="http://openweathermap.org">Open Weather Map</a> - I used Open Weather Map to search for weather data by city name.
+  </li>
+</ul>
 </div>
   )
 };
